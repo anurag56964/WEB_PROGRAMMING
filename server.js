d6f6ca3fd3a2db16f40008aa62252c0366d6b2f7
anurag56964 <anurag.case@gmail.com>
2023-09-13 21:45:18 -0400

    uploadTest

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express'); // "require" the Express module
+const app = express(); // obtain the "app" object
+const HTTP_PORT = process.env.PORT || 8080; // assign a port
+const path = require('path');
+
+// start the server on the port and output a confirmation ot the console
+app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+
+
